fcf788ab36d44a912d52862191472c1cec48e625
tomcsanyigergely <tomcsanyigergely@gmail.com>
2020-09-24 18:19:24 +0200

    Code added.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private KeyCode leftKey;
+    [SerializeField] private KeyCode rightKey;
+    [SerializeField] private KeyCode upKey;
+    [SerializeField] private KeyCode downKey;
+
+    private Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 finalVelocity = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(leftKey))
+        {
+            finalVelocity += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            finalVelocity += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(upKey))
+        {
+            finalVelocity += new Vector3(0, 0, 1);
+        }
+        else if (Input.GetKey(downKey))
+        {
+            finalVelocity += new Vector3(0, 0, -1);
+        }
+        rigidbody.velocity = finalVelocity;
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject followedPlayer;
+
+    private Collider collider;
+    private Collider followedPlayerCollider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        followedPlayerCollider = followedPlayer.GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        transform.position = followedPlayer.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == followedPlayerCollider)
+        {
+            Physics.IgnoreCollision(collider, followedPlayerCollider, true);
+            collider.isTrigger = false;
+        }
+    }
+}
